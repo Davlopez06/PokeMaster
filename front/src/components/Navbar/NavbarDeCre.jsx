517ffad9b552c0877c429aac1dc34da02338d66b
Davlopez06 <davidlopezvaldes@gmail.com>
2022-10-06 18:20:06 -0500

    Deploy

--- front/src/components/Navbar/NavbarDeCre.jsx
+++ front/src/components/Navbar/NavbarDeCre.jsx
@@ -157,7 +157,7 @@ const NavbarDeCre=()=>{
                 variant="h5"
                 noWrap
                 component="a"
-                href=""
+                href="/"
                 sx={{
                 mr: 2,
                 display: { xs: 'flex', md: 'none' },
